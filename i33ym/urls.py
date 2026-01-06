f7a01410e1935f79f51eb1b02aceb586afbe3b83
Isroil Muhitdinov <i33ym@macbook.local>
2026-01-06 19:04:38 +0500

    new flow

--- i33ym/urls.py
+++ i33ym/urls.py
@@ -11,7 +11,13 @@ urlpatterns = [
     path("react/", include("apps.reactions.urls")),
     path("newsletter/", include("apps.subscribers.urls")),
     path("rag/", include("apps.rag.urls")),
-    path("slides/", TemplateView.as_view(template_name="slides.html"), name="slides"),
+    
+    path("workshops/", TemplateView.as_view(template_name="workshops.html"), name="workshops"),
+    path("slides/", TemplateView.as_view(template_name="slides-rag.html"), name="slides"),
+    path("slides-rag/", TemplateView.as_view(template_name="slides-rag.html"), name="slides-rag"),
+    path("slides-perceptron/", TemplateView.as_view(template_name="slides-perceptron.html"), name="slides-perceptron"),
+    path("demo-perceptron/", TemplateView.as_view(template_name="demo-perceptron.html"), name="demo-perceptron"),
+    
     path("", include("apps.articles.urls")),
 ]
 
